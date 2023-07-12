6062b7d306ea143732e5bfee20d26bc2b44a37f8
Erik Hedberg <erik@hedberg.at>
2023-07-12 13:24:54 +0200

    ci: add content type to files

--- .deploy/terraform/s3.tf
+++ .deploy/terraform/s3.tf
@@ -26,3 +26,19 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
     object_ownership = "ObjectWriter"
   }
 }
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "./dist/index.html"
+  content_type = "text/html"
+  etag         = filemd5("./dist/index.html")
+}
+
+resource "aws_s3_object" "output_css" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "output.css"
+  source       = "./dist/output.css"
+  content_type = "text/css"
+  etag         = filemd5("./dist/output.css")
+}
