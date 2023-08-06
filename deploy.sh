d474f60eb23661d5ad025faa28a2d04d48d30d90
Erik Hedberg <erik@hedberg.at>
2023-08-06 15:05:36 +0200

    chore: skip requiring manual quit from deploy step

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ npm run build
 cp src/*.png dist/
 terraform apply --auto-approve
 aws s3 sync dist s3://designkod.se/ --exclude "*.map"
-aws cloudfront create-invalidation --distribution-id E2AE8XCF1HCG31 --paths "/*"
+aws cloudfront create-invalidation --distribution-id E2AE8XCF1HCG31 --paths "/*" --no-cli-pager
