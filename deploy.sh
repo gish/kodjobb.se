bd79eee90485cc4c116e6e1521de2e6376bc1af7
Erik Hedberg <erik@hedberg.at>
2023-08-06 10:53:19 +0200

    feat: build and serve with parcel

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+rm -rf dist
+npm run build
+cp src/*.png dist/
+terraform apply --auto-approve
+aws s3 sync dist s3://designkod.se/ --exclude "*.map"
+aws cloudfront create-invalidation --distribution-id E2AE8XCF1HCG31 --paths "/*"
